d4f4852357f5e19758e60fb98ae0423cba66b420
BenillaIce <antti.loytynoja@gmail.com>
2016-03-02 20:20:41 +0200

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -1,4 +1,4 @@
-# This file reads some data and plots the histogram of the global active power
+# This file reads some data and plots four different graphs
 
 # read data
 powerdata<-read.csv("household_power_consumption.txt", nrows = 100000, na.strings = "?", sep = ";")
